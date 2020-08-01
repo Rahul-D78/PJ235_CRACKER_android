d8d62ab901533830bd1eea5b811f238ffcb5b2db
Rahul kumar <rahulparida933@gamil.com>
2020-08-01 14:18:57 +0530

    Setup ViewBinding and Intents

--- app/src/main/java/com/sih/crackerandroid/MainActivity.java
+++ app/src/main/java/com/sih/crackerandroid/MainActivity.java
@@ -2,13 +2,40 @@ package com.sih.crackerandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button browseMap;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        browseMap = findViewById(R.id.browseMap);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        browseMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, AuthActivity.class);
+                startActivity(a);
+            }
+        });
+
     }
 }
